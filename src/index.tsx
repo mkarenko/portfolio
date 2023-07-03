3154dc9bba482bb2fba06ccc53c4c0a4b4e1856b
Michał Kareńko <71224089+mkarenko@users.noreply.github.com>
2023-07-03 12:39:40 +0200

    gh-update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import 'ionicons/icons';
 import './tailwindcss.css';
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename='/home'>
-			<App />
-		</BrowserRouter>
+		<App />
 	</React.StrictMode>
 );
 
--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -2,13 +2,16 @@ import {FC} from 'react';
 import {Route, Routes} from 'react-router';
 import {HomePage} from '../pages/home.page';
 import {NotFoundPage} from '../pages/notFound.page';
+import {BrowserRouter} from 'react-router-dom';
 
 const Router: FC = () => (
-	<Routes>
-		<Route path='/home' element={<HomePage />} />
+	<BrowserRouter basename='/portfolio'>
+		<Routes>
+			<Route path='/portfolio' element={<HomePage />} />
 
-		<Route element={<NotFoundPage />} />
-	</Routes>
+			<Route element={<NotFoundPage />} />
+		</Routes>
+	</BrowserRouter>
 );
 
 export default Router;
