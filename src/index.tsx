3d221fd1eb6aa01f50d24b376164c77f19c316d9
Michał Kareńko <71224089+mkarenko@users.noreply.github.com>
2023-07-03 11:51:22 +0200

    gh-page-update

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<BrowserRouter
+		//  basename={process.env.PUBLIC_URL}
+		>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
